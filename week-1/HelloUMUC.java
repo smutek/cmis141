147c7dc0a26f5ebefa8ded21f569bc282a68f199
Jimmy Smutek <jimmy@smutek.net>
2018-08-20 13:04:59 -0400

    Week 1: Homework, hello world.

---
+++ week-1/HelloUMUC.java
@@ -0,0 +1,10 @@
+/**
+ * Print "Hello UMUC!" to std out
+ */
+public class HelloUMUC
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello, UMUC!\nI'm looking forward to learning Java!");
+	}
+}
