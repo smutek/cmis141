874be2fdbf08b4158afdbf9d56db3794ba45fff6
Jimmy Smutek <jimmy@smutek.net>
2018-08-20 19:47:21 -0400

    Print lines example.

---
+++ week-1/exercises/UMUC_PrintLinesExample.java
@@ -0,0 +1,16 @@
+// File: UMUC_PrintLinesExample.java
+// Author: Jimmy Smutek
+// Date: 08-20-2018
+// Purpose: Compare print() to println()
+
+public class UMUC_PrintLinesExample
+{
+	public static void main (String[] args)
+	{
+		System.out.print ("We ");
+		System.out.println ("have");
+		System.out.print ("made contact!");
+		System.out.println ();
+
+	} // End method main
+} // End class UMUC_PrintLinesExample
