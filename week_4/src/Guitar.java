a7296ee583750c60bd24434fb5510d7a6d037a08
Jimmy Smutek <jimmy@smutek.net>
2018-09-13 12:56:12 -0400

    Done some work, commit Google Java styleguide.

--- week_4/src/Guitar.java
+++ week_4/src/Guitar.java
@@ -20,6 +20,7 @@ public class Guitar {
     this.guitarColor = guitarColor;
   }
 
+  // Getter methods for all data fields.
   public int getNumStrings() {
     return numStrings;
   }
@@ -44,10 +45,40 @@ public class Guitar {
     guitarColor = "Red";
   }
 
+  /*
+  A playGuitar() method that returns a string representation of 16 randomly selected
+musical notes of random duration. For example, the first part of the string returned
+might look like this: [A(2), G(3), B(0.5), C(1), C(1), D(0.25), ...]. You can assume
+one octave in the key of C where valid notes include A, B, C, D, E, F and G and duration
+values are .25, .5, 1, 2, and 4 representing sixteenth notes, eighth notes, quarter notes,
+half notes and whole notes, respectively.
+   */
   private String playGuitar() {
-    return "";
+
+    // array - string notes
+    String[] Notes = new String[]{"A", "B", "C", "D", "E", "F", "G"};
+    // array - double length
+    double[] NoteLength = new double[]{.25, .5, 1, 2, 4};
+    // An empty song.
+    String Song = "";
+
+    // loop 16 times
+    for (int i = 1; i <= 16; i++) {
+      String foo;
+      // array - string song
+      // select a random note
+      // select a random length
+      // append to song
+    }
+
+    // return song
+    return Song;
   }
 
+  /*
+  A toString() method that displays the number of strings, length, manufacturer and color
+  in String format
+   */
   public String toString() {
     return "Guitar{"
         + "numStrings="
