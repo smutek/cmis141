86b1eaa8728addde536caed7ae4d78aa03603e32
Jimmy Smutek <jimmy@smutek.net>
2018-09-17 13:16:02 -0400

    Week 5 discussion.

---
+++ week_5/src/ArraysDiscussion.java
@@ -0,0 +1,18 @@
+/*
+ * File: ArraysDiscussion.java
+ * Author: James Smutek
+ * Date: September 17, 2018
+ * Purpose: Provide Java code to create and initialize an array of Java primitive
+ * and demonstrate using the length member to determine the length of the array.
+ */
+public class ArraysDiscussion {
+  public static void main(String[] args) {
+
+    // Create and initialize the array with values
+    int userIDs[] = {9,11,21,32};
+
+    // Get the length of the array using the length member
+    System.out.println("The array contains " + userIDs.length  + " items.");
+
+  }
+}
